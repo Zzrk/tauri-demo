349367913b80b6502c891b2349ca7bbbd084fc77
Zzrk <1252124617@qq.com>
2023-08-31 02:01:48 +0800

    feat: splashscreen.html

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,13 +5,26 @@ mod command;
 mod menu;
 mod window;
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
-    // .setup(|app| {
-    //   // 创建外部链接的窗口
-    //   window::create_external_window_in_setup(app);
-    //   Ok(())
-    // })
+    .setup(|app| {
+      // 模拟初始化过程
+      let splashscreen_window = app.get_window("splashscreen").unwrap();
+      let main_window = app.get_window("main").unwrap();
+      tauri::async_runtime::spawn(async move {
+        println!("Initializing...");
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        println!("Done initializing.");
+        splashscreen_window.close().unwrap();
+        main_window.show().unwrap();
+      });
+
+      // 创建外部链接的窗口
+      // window::create_external_window_in_setup(app);
+      Ok(())
+    })
     .menu(menu::init_menu())
     .on_menu_event(menu::handle_menu_event)
     .manage(command::Database {})
